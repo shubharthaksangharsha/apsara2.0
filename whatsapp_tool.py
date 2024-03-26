f47819d761d7dc6544247214673c41aaf96f9125
shubharthaksangharsha <shubharthaksangharsha@gmail.com>
2024-03-26 17:26:54 +0530

    Fixed load_dot_env()

--- whatsapp_tool.py
+++ whatsapp_tool.py
@@ -4,7 +4,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from undetected_chromedriver import Chrome
 from helper_selenium_functions import * 
 from webdriver_manager.chrome import ChromeDriverManager
 from langchain.tools import tool
