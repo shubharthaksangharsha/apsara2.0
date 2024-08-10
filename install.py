a6cb529863b462d94f1eae0988883667bab78833
shubharthaksangharsha <shubharthaksangharsha@gmail.com>
2024-08-11 03:51:50 +0530

    Fixed install.py again

--- install.py
+++ install.py
@@ -2,13 +2,12 @@ import subprocess
 
 # List of packages to install using apt-get
 apt_packages = [
-    "libgirepository1.0-dev",
     "libasound-dev",
     "portaudio19-dev",
     "libportaudio2",
     "libportaudiocpp0",
-    "libgirepository1.0-dev",
     "gobject-introspection",
+    "libgirepository1.0-dev",
     "pulseaudio", 
     "ffmpeg",
     "bluez", 
@@ -17,21 +16,30 @@ apt_packages = [
 ]
 
 # Check if apt is not locked
+print('Checking if any apt is not locked')
 subprocess.run(['sudo', 'apt', 'install', '--fix-broken', '-y'], check=True)
 # Install each package using apt-get
 for package in apt_packages:
-    print('PACKAGE INSTALLING:', package)
-    if package == 'libgirepository1.0-dev':
+    if package == 'gobject-introspection':
+        print('Breaking...')
         break
+    print('PACKAGE INSTALLING:', package)
     subprocess.run(["sudo", "apt-get", "install", "-y", package], check=True)
 
+print('Updating packages...')
 subprocess.run(["sudo", "apt", "update", "-y"], check=True)
+print('Installing pyaudio')
+subprocess.run(["pip", "install", "pyaudio"])
+print('Continue installing other packages...')
+for package in apt_packages[apt_packages.index('gobject-introspection'):]:
+    subprocess.run(["sudo", "apt-get", "install", "-y", package], check=True)
+
+print('INSTALLING PACKAGES FROM requirements.txt')
 # Install Python packages listed in requirements.txt using pip
 subprocess.run(["pip", "install", "-r", "requirements.txt"], check=True)
 
-for package in apt_packages[apt_packages.index('libgirepository1.0-dev'):]:
-    subprocess.run(["sudo", "apt-get", "install", "-y", package], check=True)
-
+print('INSTALLING PLAYWRIGHT DEPENDENCIES')
 #Playwright 
 subprocess.run(["playwright", "install"], check=True)
 subprocess.run(["playwright", "install-deps"], check=True)    
+
